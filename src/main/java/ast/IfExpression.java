9c5cdbc7ba7300e4b44d6e729bfdf01f69476608
IcePanorama <thriftedroot@protonmail.com>
2024-04-17 20:37:59 -0400

    Created IfExpression class

---
+++ src/main/java/ast/IfExpression.java
@@ -0,0 +1,33 @@
+package ast;
+
+import token.Token;
+
+public class IfExpression implements Expression{
+    Token token;
+    Expression condition;
+    BlockStatement consequence;
+    BlockStatement alternative;
+
+    public IfExpression() {}
+
+    public void expressionNode() {}
+
+    public String TokenLiteral() {
+        return token.literal;
+    }
+
+    public String toString() {
+        String output = "if";
+
+        output += condition.toString();
+        output += " ";
+        output += consequence.toString();
+
+        if (alternative != null) {
+            output += "else ";
+            output += alternative.toString();
+        }
+
+        return output;
+    }
+}
