ff645286da70844fa97dac59ec6c0e3f750fece2
IcePanorama <thriftedroot@protonmail.com>
2024-04-18 20:52:35 -0400

    Created CallExpression class.

---
+++ src/main/java/ast/CallExpression.java
@@ -0,0 +1,34 @@
+package ast;
+
+import java.util.ArrayList;
+
+import token.Token;
+
+public class CallExpression implements Expression{
+    Token token;
+    public Expression function;
+    public ArrayList<Expression> arguements;
+
+    public CallExpression() {}
+
+    public void expressionNode() {}
+
+    public String TokenLiteral() {
+        return token.literal;
+    }
+
+    public String toString() {
+        String output = function.toString();
+
+        String[] args = new String[arguements.size()];
+        for (int i = 0; i < arguements.size(); i++) {
+            args[i] = arguements.get(i).toString();
+        }
+
+        output += "(";
+        output += String.join(", ", args);
+        output += ")";
+
+        return output;
+    }
+}
