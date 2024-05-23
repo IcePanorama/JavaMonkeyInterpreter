a31162eddc7c5eaed609a7e9d3ad6cb32fe012ae
IcePanorama <thriftedroot@protonmail.com>
2024-05-23 14:51:00 -0400

    Created Function object class.

---
+++ src/main/java/monkeyobject/MonkeyFunction.java
@@ -0,0 +1,25 @@
+package monkeyobject;
+
+import java.util.ArrayList;
+
+import ast.BlockStatement;
+import ast.Identifier;
+
+public class MonkeyFunction implements MonkeyObject{
+    public final static String FUNCTION_OBJ = "FUNCTION";
+    ArrayList<Identifier> parameters;
+    BlockStatement body;
+    Environment env;
+
+    public String Type() { return FUNCTION_OBJ; }
+    public String Inspect() {
+        String[] params = new String[parameters.size()];
+        for (int i = 0; i < parameters.size(); i++) {
+            params[i] = parameters.get(i).toString();
+        }
+
+        String output = "fn (";
+        output += String.join(", ", params);
+        return output + String.format(") {\n%s\n}", body.toString());
+    }
+}
