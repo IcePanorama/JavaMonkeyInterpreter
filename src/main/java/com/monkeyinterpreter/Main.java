262e9d54fef751aaacb0b529e13f5df5f9555986
IcePanorama <thriftedroot@protonmail.com>
2024-05-23 13:53:38 -0400

    Changed the formatting of the program start message.

--- src/main/java/com/monkeyinterpreter/Main.java
+++ src/main/java/com/monkeyinterpreter/Main.java
@@ -4,7 +4,7 @@ import repl.REPL;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello! Welcome to the Monkey Programming Language!");
+        System.out.println("Hello! Welcome to the Monkey programming language!");
         System.out.println("Feel free to type in commands.");
         REPL.Start();
     }
